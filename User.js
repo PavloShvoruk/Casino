8266688f0a1750a9b860a8871282992bfad19e13
PavloShvoruk <duckdogers07@gmail.com>
2019-07-10 19:25:44 +0300

    User small fixes

--- User.js
+++ User.js
@@ -1,16 +1,29 @@
+import { findCasino, findMachine } from "./Helpers.js";
 export class User {
-    constructor(name, money) {
-        this.name = name;
-        this.money = money;
-    }
+  constructor(name, money) {
+    this.name = name;
+    this.money = money;
+  }
 
-    play(money, casinoName, i) {
-        if (this.money === 0) {
-            throw "Sorry, you don't have money to play"
-        } else if ((this.money - money) < 0) {
-            throw "Sorry, you don't have enough money to play"
-        } else {
-            casinoName.machines[i].play(money);
-        }
+  play(casinoOwner, casinoName, money) {
+    if (money === 0 || isNaN(money)) {
+      throw new Error("Not valid bet!");
     }
+    if (this.money - money < 0) {
+      throw new Error("You don't have enough money to bet!");
+    } else {
+      try {
+        let casino = findCasino(casinoOwner, casinoName);
+        //random choose of machine
+        let randomMachineNumber =
+          Math.floor(Math.random() * casino.getMachineCount) + 1;
+        let machine = findMachine(casinoOwner, casinoName, randomMachineNumber);
+        this.money -= money;
+        this.money += machine.play(money);
+      } catch (error) {
+        console.log(error.message);
+        this.money += money;
+      }
+    }
+  }
 }
