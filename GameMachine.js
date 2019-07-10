e2ee0a180c2f00298160183f4e7b52b38f18709e
PavloShvoruk <duckdogers07@gmail.com>
2019-07-10 19:04:35 +0300

    game machine mini fixes

--- GameMachine.js
+++ GameMachine.js
@@ -10,18 +10,28 @@ export class GameMachine {
     takeMachineMoney(number) {
         if ((this.number - number) < 0) {
             //TODO: notification that amount to be taken larger
-            this.number -= this.getMoney;
-        } else {
             this.number -= number;
+        } else {
+            throw new Error('Not enough money on this machine.')
         }
     }
 
     putMoney(number) {
-        this.number += number;
+        if (isNaN(number)) {
+            throw new Error('Cannot put such amount to machine');
+        } else {
+            this.number += number;
+        }
     }
 
     play(number) {
-        this.number += number;
+        if (isNaN(number)) {
+            throw new Error('Cannot put such amount to machine');
+        } else if ((this.number - number) < 0) {
+            throw new Error("Machine does not have enough money to play. Please contact admin!");
+        } else {
+            this.number += number;
+        }
         let resultArray = [];
 
         //push three random numbers to array in range between 0 and 10
@@ -31,7 +41,7 @@ export class GameMachine {
         console.log(resultArray);
         //for test
         // resultArray = [7, 7, 7];
-        //count occurence of each number in array 
+        //count occurence of each number in resultArray 
         const occurence = resultArray.reduce((acc, el) => {
             acc[el] = (acc[el] || 0) + 1;
             return acc;
@@ -42,14 +52,16 @@ export class GameMachine {
             if (occurence[prop] === 2) {
                 console.log("double win");
                 //TODO: amount  of money in machine cheker
-                this.number -= number * 2;
+                this.takeMachineMoney(number * 2);
                 return number * 2;
             }
             if (occurence[prop] === 3) {
                 console.log("triple win");
                 //TODO: amount  of money in machine cheker
-                this.number -= number * 3;
+                this.takeMachineMoney(number * 3);
                 return number * 3;
+            } else {
+                return 0;
             }
         }
     }
